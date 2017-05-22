57ba14bea229ed751d066247f194bd3a3ae8d161
mskalbania <cod634.steam@gmail.com>
2017-05-22 21:25:15 +0200

    v1.1

--- QuickSort/src/QuickSort.java
+++ QuickSort/src/QuickSort.java
@@ -1,6 +1,16 @@
+import java.util.List;
+
 public class QuickSort {
 
 
-
+    public static double MiddleValue(List<Integer> input) {
+        double output;
+        if (input.size() % 2 == 0) {
+            output = ((double)(input.get((input.size() / 2) - 1) + input.get(input.size() / 2)))/2;
+        } else {
+            output = input.get(input.size() / 2);
+        }
+        return output;
+    }
 
 }
--- QuickSort/src/Run.java
+++ QuickSort/src/Run.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Run {
     public static void main(String[] args) {
 
+        List<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(5);
+        list.add(7);
+        list.add(5);
+        list.add(9);
+        list.add(10);
+
+        System.out.println(QuickSort.MiddleValue(list));
     }
 }
